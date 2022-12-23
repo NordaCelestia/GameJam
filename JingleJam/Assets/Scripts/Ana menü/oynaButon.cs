84d249f12c1fbefba8147444daa373e8c586b9f5
NordaCelestia <fatihkocaman797@gmail.com>
2022-12-23 18:31:29 +0300

    Ana Menü butonları

---
+++ JingleJam/Assets/Scripts/Ana menü/oynaButon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class oynaButon : MonoBehaviour
+{
+    
+    public void oynaButonu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
