2432f553374a04018182b61292907dd74b25ae67
NordaCelestia <fatihkocaman797@gmail.com>
2022-12-25 00:23:28 +0300

    2. evdeki konuşmaları ypatım

--- JingleJam/Assets/Scripts/Dışarııs/ikinicEv_Konusma.cs
+++ JingleJam/Assets/Scripts/Dışarııs/ikinicEv_Konusma.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ikinicEv_Konusma : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject canvas;
+    public TextMeshProUGUI isim;
+    public TextMeshProUGUI konusma;
+
+    private void Start()
     {
-        
+        StartCoroutine(ikinciEvGiris());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ikinciEvGiris()
     {
-        
+        isim.text = "Ellie";
+        konusma.text = "Harika iþ çýkardýn Nicholas!  Þimdi Noel'in olmazsa olmazý hediyeleri hazýrlamaya baþladýk";
+        yield return new WaitForSeconds(6);
+
+        isim.text = "Nicholas";
+        konusma.text = "Tabi ya hediye! Ellie? Noel babanýn ren geyikleri nerde?";
+        yield return new WaitForSeconds(5);
+
+        isim.text = "Ellie";
+        konusma.text = "En son bahçede gördüm.";
+        yield return new WaitForSeconds(6);
+
+
     }
+
 }
