b273d0ab5c6f8155321b7a58db889c976dad2d46
NordaCelestia <fatihkocaman797@gmail.com>
2022-12-24 18:56:50 +0300

    ilerledik

---
+++ JingleJam/Assets/Scripts/Ev ici/evCik.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class evCik : MonoBehaviour
+{
+    
+    public void evCikisss()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ JingleJam/Assets/Scripts/Ev ici/evdenCik.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class evdenCik : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+               
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (true)
+            {
+                isInRange = true;
+                Debug.Log("Player is in range");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player is not in range");
+        }
+    }
+}
